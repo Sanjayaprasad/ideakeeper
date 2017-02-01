8b34b3f07a59735336baf684e4d5849d207e61f1
Sanju <sanjuap800@gmail.com>
2017-02-01 15:25:02 +0530

    Revert Changes in test file and checking in

--- spec/ideakeepertest.js
+++ spec/ideakeepertest.js
@@ -5,7 +5,7 @@ var webdriver = require('selenium-webdriver'),
 
 var driver = new webdriver.Builder().forBrowser('chrome').build();
 
-driver.get('http://localhost:3000/api');
+driver.get('http://localhost:3000');
 driver.findElement(By.id('text')).sendKeys('get idea');
 driver.findElement(By.id('click')).click();
 driver.quit();
