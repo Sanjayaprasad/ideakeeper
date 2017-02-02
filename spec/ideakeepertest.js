3138516f25b18346ce8a21a53279860e86d0934c
Sanju <sanjuap800@gmail.com>
2017-02-02 15:00:19 +0530

    Making changes in test file and checking in

--- spec/ideakeepertest.js
+++ spec/ideakeepertest.js
@@ -2,10 +2,18 @@
 var webdriver = require('selenium-webdriver'),
     By = require('selenium-webdriver').By,
     until = require('selenium-webdriver').until;
-
+var assert = require('assert');
+var expect = require('expect');
 var driver = new webdriver.Builder().forBrowser('chrome').build();
 
 driver.get('http://localhost:3000');
 driver.findElement(By.id('text')).sendKeys('get idea');
-driver.findElement(By.id('click')).click();
+driver.findElement(By.id('click'))
+.click().then(function(){
+    return driver.findElement(By.id("text")).getAttribute("value");
+})
+.then(function (value) {
+    expect("get idea").toEqual('get idea');
+    //done();
+});
 driver.quit();
